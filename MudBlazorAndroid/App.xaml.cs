cc307d21670149a263e0c70d0558d8545e3e4f98
Mike Andrews <andrews.mike90@gmail.com>
2022-12-29 13:50:05 +0000

    Add project files.

---
+++ MudBlazorAndroid/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MudBlazorAndroid;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MudBlazorAndroid/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MudBlazorAndroid;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
